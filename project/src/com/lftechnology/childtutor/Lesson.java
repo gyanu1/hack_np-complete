8b358fd925484f8b24dc19626476a16cc38b9bd4
Sudhir Shakya <sudhirshakya@lftechnology.com>
2013-05-11 09:51:14 +0545

    Added POJO class for Lesson.

---
+++ project/src/com/lftechnology/childtutor/Lesson.java
@@ -0,0 +1,35 @@
+package com.lftechnology.childtutor;
+
+import java.util.List;
+
+public class Lesson {
+
+    private int currentPage;
+
+    public static class Page {
+        String image;
+        String sound;
+    }
+
+    private List<Page> pages;
+
+    public Page getNextPage() {
+        if (isLastPage())
+            return null;
+        return pages.get(++currentPage);
+    }
+
+    public Page getPrevPage() {
+        if (isFirstPage())
+            return null;
+        return pages.get(--currentPage);
+    }
+
+    public boolean isLastPage() {
+        return currentPage == pages.size() - 1;
+    }
+
+    public boolean isFirstPage() {
+        return currentPage == 0;
+    }
+}
