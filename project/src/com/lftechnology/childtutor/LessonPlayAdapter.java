6ea1614c24e2b8a38b43f36409ddb3576694587c
gyanu1 <gmaharjan@lftechnology.com>
2013-05-11 16:03:01 +0000

    working code restored

--- project/src/com/lftechnology/childtutor/LessonPlayAdapter.java
+++ project/src/com/lftechnology/childtutor/LessonPlayAdapter.java
@@ -5,22 +5,11 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.lftechnology.childtutor.model.Lesson;
 
 public class LessonPlayAdapter extends PagerAdapter {
 	Context context=null;
-	Lesson data=null;
-	static class ViewHolder {
-         String soundPath;
-		 ImageView image;
-	}
-	
-	public LessonPlayAdapter(Context context, Lesson data){
+	public LessonPlayAdapter(Context context){
 		this.context=context;
-		this.data=data;
 	}
 
 	@Override
@@ -34,34 +23,35 @@ public class LessonPlayAdapter extends PagerAdapter {
 		// TODO Auto-generated method stub
 		return arg0 == ((View) arg1);
 	}
-	
-	public Object instantiateItem(View convertView, int position) {
-		 
-		ViewHolder holder;
-		Lesson.Page page;
-		if (convertView == null) { // if it's not recycled, initialize some
-									// attributes
-			page=this.data.getNextPage() ;
-			while(page != null){
-			LayoutInflater inflater = (LayoutInflater) convertView.getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			convertView = inflater.inflate(R.layout.current_page, null);
-			holder = new ViewHolder();			
-			holder.image = (ImageView) convertView.findViewById(R.id.current_image);
-			//holder.image.setImageURI(R.drawable.logo);
-			//holder.image.setBackground(this.context.getResources().getDrawable(R.drawable.logo));
-			convertView.setTag(holder);
-			}
 
-		} else {
-			holder = (ViewHolder) convertView.getTag();
+	public Object instantiateItem(View collection, int position) {
+
+		LayoutInflater inflater = (LayoutInflater) collection.getContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		int resId = 0;
+		switch (position) {
+		case 0: {
+			resId = R.layout.previous_page;
+			break;
 		}
-//		holder.text.setText(label[position]);
-//		holder.text.setCompoundDrawablesWithIntrinsicBounds(BankTransactionAdapter.this.mThumbIds[position], 0, 0, 0);
-//	//	holder.icon.setImageResource(mThumbIds[position]);
-		return convertView;
-	}
+		case 1: {
+			resId = R.layout.current_page;
+			break;
+		}
+		case 2: {
+			resId = R.layout.next_page;
+			break;
+		}
+		}
 
+		View view = inflater.inflate(resId, null);
+ 
+		((ViewPager) collection).addView(view, 0);
+ 
+		return view;
+	}
+ 
 	@Override
 	public void destroyItem(View arg0, int arg1, Object arg2) {
 		((ViewPager) arg0).removeView((View) arg2);
