5d853372e83b70bce150a2bdd7e5ddce33971a88
gyanu1 <gmaharjan@lftechnology.com>
2013-05-11 09:42:14 +0000

    lesson1 and lesson2 button added in homepage

--- project/src/com/lftechnology/childtutor/Home.java
+++ project/src/com/lftechnology/childtutor/Home.java
@@ -1,8 +1,10 @@
 package com.lftechnology.childtutor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class Home extends Activity {
 
@@ -10,6 +12,10 @@ public class Home extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		View lesson1Button = findViewById(R.id.lesson1);
+		lesson1Button.setOnClickListener((OnClickListener) Home.this);
+		View lesson2Button = findViewById(R.id.lesson2);
+		lesson2Button.setOnClickListener((OnClickListener) Home.this);
 	}
 
 	@Override
@@ -17,6 +23,20 @@ public class Home extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.lesson1:
+			//call lessen1
+			break;
+
+		case R.id.lesson2:
+			//call lesson2
+			break;
+
+		}
 	}
 
 }
