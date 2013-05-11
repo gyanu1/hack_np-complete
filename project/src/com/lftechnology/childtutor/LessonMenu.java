3369f4d27bac20c56b6a3cfbc834f8a8af29a4d7
Sudhir Shakya <sudhirshakya@lftechnology.com>
2013-05-11 12:18:13 +0545

    Added feature to pass lesson id from menu to lesson activity.

--- project/src/com/lftechnology/childtutor/LessonMenu.java
+++ project/src/com/lftechnology/childtutor/LessonMenu.java
@@ -4,13 +4,12 @@
 package com.lftechnology.childtutor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-
-import com.lftechnology.childtutor.utils.AndroidUtility;
 
 /**
  * @author navaraj
@@ -18,43 +17,47 @@ import com.lftechnology.childtutor.utils.AndroidUtility;
  */
 public class LessonMenu extends Activity implements OnClickListener {
 
-    private static final String TAG = "LessonMenu";
+	private static final String TAG = "LessonMenu";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        Log.d(TAG, "Creating lesson");
+		Log.d(TAG, "Creating lesson");
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.lesson_activity);
-        View lesson1Button = findViewById(R.id.lesson1);
-        lesson1Button.setOnClickListener(LessonMenu.this);
-        View lesson2Button = findViewById(R.id.lesson2);
-        lesson2Button.setOnClickListener(LessonMenu.this);
-    }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lesson_activity);
+		View lesson1Button = findViewById(R.id.lesson1);
+		lesson1Button.setOnClickListener(LessonMenu.this);
+		View lesson2Button = findViewById(R.id.lesson2);
+		lesson2Button.setOnClickListener(LessonMenu.this);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
 
-    @Override
-    public void onClick(View v) {
-        AndroidUtility.play(this, R.raw.beep9); // To generate sound
-                                                            // while clicking
-                                                            // button
+	@Override
+	public void onClick(View v) {
+		Intent intent;
+		Bundle bundle;
 
-        switch (v.getId()) {
+		intent = new Intent().setClass(this, LessonPlay.class);
+		bundle = new Bundle();
 
-        case R.id.lesson1:
-            // call lessen1
-            break;
-        case R.id.lesson2:
-            // call lessen1
-            break;
+		switch (v.getId()) {
 
-        }
-    }
+		case R.id.lesson1:
+			bundle.putInt("lesson", R.raw.kakhaga);
+			break;
+		case R.id.lesson2:
+			bundle.putInt("lesson", R.raw.kakhaga);
+			break;
+
+		}
+		intent.putExtras(bundle);
+		startActivity(intent);
+	}
 }
--- project/src/com/lftechnology/childtutor/LessonPlay.java
+++ project/src/com/lftechnology/childtutor/LessonPlay.java
@@ -1,22 +1,50 @@
 package com.lftechnology.childtutor;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
-public class LessonPlay extends Activity{
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.lesson_play);
+import com.lftechnology.childtutor.model.Lesson;
+import com.lftechnology.childtutor.model.LessonParser;
 
-		// Create and set adapter
-		LessonPlayAdapter adapter = new LessonPlayAdapter(this);
-		ViewPager myPager = (ViewPager) findViewById(R.id.customviewpager);
-		myPager.setAdapter(adapter);
-		myPager.setCurrentItem(0);
-		
-		
-	}
+public class LessonPlay extends Activity {
+
+    private Lesson lesson;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson_play);
+
+        // Create and set adapter
+        LessonPlayAdapter adapter = new LessonPlayAdapter(this);
+        ViewPager myPager = (ViewPager) findViewById(R.id.customviewpager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+
+        loadLesson();
+
+    }
+
+    private void loadLesson() {
+        try {
+            Bundle bundle = getIntent().getExtras();
+            int lessonId = bundle.getInt("lesson");
+
+            InputStream is = this.getResources().openRawResource(lessonId);
+            lesson = LessonParser.parse(is);
+        } catch (XmlPullParserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
